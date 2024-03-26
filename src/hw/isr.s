5b8b69498ff34edad4b5bf342180e3abee1a6cce
Luna Saphie Mittelbach <lunarlambda@gmail.com>
2024-03-26 14:32:11 +0100

    fix(isr): don't rely on r0 having a specific value on entry.

--- src/hw/isr.s
+++ src/hw/isr.s
@@ -20,49 +20,53 @@
 @ r2: <scratch>
 @ r3: Handler function
 fn isrDefault arm
+    @ The GBA BIOS always calls the IRQ handler with r0 = 0x04000000,
+    @ however this is unreliable in emulators/GBARunner/etc.
+    mov         r0, 0x04000000                                  @ 1
+
     @ Read IE & IF
-    ldr         r1, [r0, OFF_IE]!                               @ 3
-    and         r1, r1, r1, lsr 16                              @ 4
+    ldr         r1, [r0, OFF_IE]!                               @ 4
+    and         r1, r1, r1, lsr 16                              @ 5
 
     @ Handle the lowest set IRQ
-    rsb         r2, r1, 0                                       @ 5
-    and         r1, r1, r2                                      @ 6
+    rsb         r2, r1, 0                                       @ 6
+    and         r1, r1, r2                                      @ 7
 
     @ Acknowlegde IF
-    strh        r1, [r0, OFF_IE_IF]                             @ 8
+    strh        r1, [r0, OFF_IE_IF]                             @ 9
 
     @ Acknowledge IFBIOS
-    ldr         r2, [r0, OFF_IE_IFBIOS]!                        @ 11
-    orr         r2, r2, r1                                      @ 12
-    strh        r2, [r0], 8                                     @ 14
+    ldr         r2, [r0, OFF_IE_IFBIOS]!                        @ 12
+    orr         r2, r2, r1                                      @ 13
+    strh        r2, [r0], 8                                     @ 15
 
     @ Lookup the IRQ handler using a 16-bit Debruijn sequence.
-    mov         r0, 0x09 << 24                                  @ 15
-    orr         r0, 0xAF << 16                                  @ 16
-    mul         r2, r0, r1                                      @ 18 / 19
-    lsr         r2, r2, 28                                      @ 20
+    mov         r0, 0x09 << 24                                  @ 16
+    orr         r0, 0xAF << 16                                  @ 17
+    mul         r2, r0, r1                                      @ 19 / 20
+    lsr         r2, r2, 28                                      @ 21
 
     @ Load handler
-    adr         r3, ISR_DEFAULT_HANDLERS                        @ 21
-    ldr         r3, [r3, r2, lsl 2]                             @ 24
-    cmp         r3, 0                                           @ 25
-    bxeq        lr                                              @ 26 / 28
+    adr         r3, ISR_DEFAULT_HANDLERS                        @ 22
+    ldr         r3, [r3, r2, lsl 2]                             @ 25
+    cmp         r3, 0                                           @ 26
+    bxeq        lr                                              @ 27 / 29
 
     @ Enable nesting
-    mrs         r2, spsr                                        @ 27
-    msr         cpsr_c, 0x9F                                    @ 28
-    push        {r2, lr}                                        @ 31
+    mrs         r2, spsr                                        @ 28
+    msr         cpsr_c, 0x9F                                    @ 29
+    push        {r2, lr}                                        @ 32
 
     @ Call
-    mov         r0, r1                                          @ 32
-    mov         lr, pc                                          @ 33
-    bx          r3                                              @ 36
+    mov         r0, r1                                          @ 33
+    mov         lr, pc                                          @ 34
+    bx          r3                                              @ 37
 
     @ Return
-    pop         {r2, lr}                                        @ 40
-    msr         cpsr_c, 0x92                                    @ 41
-    msr         spsr, r2                                        @ 42
-    bx          lr                                              @ 45
+    pop         {r2, lr}                                        @ 41
+    msr         cpsr_c, 0x92                                    @ 42
+    msr         spsr, r2                                        @ 43
+    bx          lr                                              @ 46
 data ISR_DEFAULT_HANDLERS global .inline=1
     .fill       16, 4, 0
 endd
@@ -73,26 +77,28 @@ endfn
 @ r2: <scratch>
 @ r3: Handler function
 fn isrMinimal arm
+    mov         r0, 0x04000000                                  @ 1
+
     @ Read IE & IF
-    ldr         r1, [r0, #OFF_IE]!                              @ 3
-    and         r1, r1, r1, lsr #16                             @ 4
+    ldr         r1, [r0, #OFF_IE]!                              @ 4
+    and         r1, r1, r1, lsr #16                             @ 5
 
     @ Acknowledge IF
-    strh        r1, [r0, #OFF_IE_IF]                            @ 6
+    strh        r1, [r0, #OFF_IE_IF]                            @ 7
 
     @ Acknowledge IFBIOS
-    ldr         r2, [r0, #OFF_IE_IFBIOS]!                       @ 9
-    orr         r2, r2, r1                                      @ 10
-    strh        r2, [r0], 8                                     @ 12
+    ldr         r2, [r0, #OFF_IE_IFBIOS]!                       @ 10
+    orr         r2, r2, r1                                      @ 11
+    strh        r2, [r0], 8                                     @ 13
 
     @ Load handler
-    ldr         r3, ISR_MINIMAL_HANDLER                         @ 15
-    cmp         r3, 0                                           @ 16
-    bxeq        lr                                              @ 17 / 19 (exit)
+    ldr         r3, ISR_MINIMAL_HANDLER                         @ 16
+    cmp         r3, 0                                           @ 17
+    bxeq        lr                                              @ 18 / 20 (exit)
 
     @ Call
-    mov         r0, r1                                          @ 18
-    bx          r3                                              @ 21
+    mov         r0, r1                                          @ 19
+    bx          r3                                              @ 22
 data ISR_MINIMAL_HANDLER global .inline=1
     .word       0
 endd
